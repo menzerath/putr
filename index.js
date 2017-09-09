3e887064b3e6c58a80bae3f223bc6134d2cafda1
Marvin Menzerath <github@marvin-menzerath.de>
2017-09-09 21:38:29 +0200

    Simplified database transactions

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const config = require("./config/local.json");
-const database = require("./lib/database.js");
 const logger = require("./lib/logging.js");
 const pkg = require("./package.json");
 const putr = require("./lib/putr.js");
 
 // init
 logger.info("Welcome to putr v%s", pkg.version);
-
-// check database connection
-database.getConnection((err, connection) => {
-	if (err) {
-		logger.error("No database connection (%s)", err);
-		process.exit(1);
-	}
-});
 
 // setup http-server
 const express = require("express");
